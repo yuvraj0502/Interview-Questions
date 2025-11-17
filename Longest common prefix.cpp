5de8d88616c32d47ed727a153aa758f05bb4889f
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-11-17 16:55:19 +0530

    Longest common prefix

---
+++ Longest common prefix.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()){
+            return "";
+        }
+        string prfix=strs[0];
+        for(int i=1;i<strs.size();i++){
+            while(strs[i].find(prefix)!=0){
+                prefix=prefix.substr(0,prefix.length()-1);
+                if(prefix.empty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+
+    }
+}
