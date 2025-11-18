f04b7345c85f341a75ab457447d23469c6f93783
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-11-18 11:48:50 +0530

    Transpose matrix

---
+++ Transpose matrix.cpp
@@ -0,0 +1,10 @@
+class Solution{
+    public:
+    void transpose(vector<vector<int>>&matrix){
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
