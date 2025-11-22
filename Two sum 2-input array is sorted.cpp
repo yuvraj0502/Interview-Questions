bd5c5a6ef6aa1bd77f1e930c32dd7b7394e12252
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-11-22 22:39:44 +0530

    Two sum 2-input array is sorted

---
+++ Two sum 2-input array is sorted.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        int start=0;
+        int end=nums.size()-1;
+
+        while(start<=end){
+            int sum=nums[start]+nums[end];
+            if(sum==target){
+                return {start+1,end+1};
+            }
+            else if(sum<target){
+                start++;
+            }
+            else{
+                end--;
+            }
+        }
+        return {-1,-1};
+    }
+}
