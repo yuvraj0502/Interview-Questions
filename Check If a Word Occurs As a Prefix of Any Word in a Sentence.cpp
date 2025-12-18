afad9155fb4ae60aa71ce09d02747043f53d23d6
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-18 17:56:57 +0530

    Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    int isPrefixOfWord(string sentence, string searchWord){
+        istringstream iss(sentence);
+        string word;
+        int index = 1;
+
+        while (iss >> word) {
+           if(word.find(searchWord) == 0){
+               return index;
+           }
+           index++;
+        }r
+        return -1;
+    }
+}
