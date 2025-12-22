102aa65d62a41fe160df441d0ded319ada94c7ee
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-22 16:35:31 +0530

    Array Partition

--- Array Partition.cpp
+++ Array Partition.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int i(vector<int>& nums) {
+    int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int maxSum=0;
         for(int i=0;i<nums.size();i+=2){
