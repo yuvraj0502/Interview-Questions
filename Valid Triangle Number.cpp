77e7395958235c8a71417d458ee9f3ea3c1549a2
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-22 17:14:52 +0530

    Valid Triangle Number

---
+++ Valid Triangle Number.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    int triangleNumber(vector<int.&nums){
+        sort(nums.begin(),nums.end());
+        int count=0;
+        for(int i=nums.size()-1;i>=2;i--){
+            int left=0;
+            int right=i-1;
+            while(left<right){
+                if(nums[left]+nums[right]>nums[i]){
+                    count+=right-left;
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+}
