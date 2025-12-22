72640864af2bffc02aa9928f36ce4044e8e00a70
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-22 16:48:16 +0530

    Can Place Flowers

---
+++ Can Place Flowers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool (vector<int>& flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 0) {
+                bool leftside = (i == 0) || flowerbed[i - 1] == 0;
+                bool rightside =
+                    (i == flowerbed.size() - 1) || flowerbed[i + 1] == 0;
+
+                if (leftside && rightside) {
+                    flowerbed[i] = 1;
+                    n--;
+
+                    if (n == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
