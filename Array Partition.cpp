25d34314ae9526cd7d7d56f30c048f18ccb5114e
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-22 11:13:54 +0530

    Array Partition

---
+++ Array Partition.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int i(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int maxSum=0;
+        for(int i=0;i<nums.size();i+=2){
+            maxSum+=nums[i];
+        }
+        return maxSum;
+    }
+};
