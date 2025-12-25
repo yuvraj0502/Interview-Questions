b798e1123ea709c663ff04784e3998639fd2c334
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-25 20:55:18 +0530

    Level Order Traversal (Breadth First Search) of Binary Tree

---
+++ Level Order Traversal (Breadth First Search) of Binary Tree.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    void levelOrder(TreeNode* root,int level,vector<vector<int>>&result){
+        if(root==nullptr){
+            return;
+        }
+        if(result.size()==lelel){
+            result.push_back({});
+        }
+        result[level].push_back(root->data);
+        levelOrder(root->left,level+1,result);
+        levelOrder(root->right,level+1,result);
+    };
+
+    vector<vector<int>r>levelorderTraversal(TreeNode* root){
+        if(root==nullptr){
+            return {};
+        }
+        vector<vector<int>>result;
+        levelOrder(root,0,result);
+        return result;
+    }
+};
