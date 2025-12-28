7c3dda76e4417598faf999027f35a537ece1091c
Yuvraj Gupta <yuvrajguptaa0502@gmail.com>
2025-12-28 13:20:23 +0530

    Count Complete Tree Nodes

---
+++ Count Complete Tree Nodes.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    int countNodes(TreeNode* root){
+        if(root==nullptr){
+            return 0;
+        }
+        int leftCount=countNodes(root->left);
+        int rightCount=countNodes(root->right);
+        return leftCount+rightCount+1;
+    }
+}
